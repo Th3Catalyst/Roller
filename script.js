2eebe058df70e804b2dad6bb7f9de27dd15f768a
Th3Catalyst <max675295@gmail.com>
2025-04-26 00:18:34 -0700

    optimizations

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let extraTimesL = [];
 let running;
 let total;
 let bar;
+let done;
 
 function weightedRandom(list) {
 
@@ -22,6 +23,17 @@ function weightedRandom(list) {
         };
     };
 };
+
+function checkFlag(flag, func) {
+    if (!eval(flag)) {
+        console.log(flag);
+        setTimeout(() => {checkFlag(flag,func)},100);
+    } else {
+        console.log(func)
+        func();
+    }
+};
+
 let values = [
     {value: 0, rarity: 0.000001},
     {value: 0.00001, rarity: 0.1},
@@ -30,16 +42,16 @@ let values = [
     {value: 0.05, rarity: 0.25},
     {value: 0.1, rarity:2},
     {value: 0.2, rarity: 3},
-    {value: 0.3, rarity: 4},
     {value: 0.5, rarity: 6},
-    {value: 0.7, rarity: 35},
-    {value: 0.8, rarity: 25},
-    {value: 1, rarity: 15},
+    {value: 0.7, rarity: 25},
+    {value: 0.8, rarity: 20},
+    {value: 1, rarity: 30},
     {value: 1.5, rarity: 5},
-    {value: 2, rarity: 2},
-    {value: 3, rarity: 1},
-    {value: 4, rarity: 0.75},
-    {value: 5, rarity: 0.5},
+    {value: 2, rarity: 4},
+    {value: 3, rarity: 2},
+    {value: 4, rarity: 1},
+    {value: 5, rarity: 0.75},
+    {value: 6, rarity: 0.5},
     {value: 10, rarity: 0.1},
     {value: 20, rarity: 0.01},
     {value: 50, rarity: 0.001},
@@ -49,6 +61,13 @@ let values = [
     {value: 1000, rarity: 0.000001},
     
 ];
+
+let totalPercent = 0;
+for (i=0;i<values.length;i++) {
+    totalPercent += values[i].rarity;
+};
+console.log(totalPercent);
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -75,14 +94,15 @@ async function Roll(betVal = 0, div = "output1") {
         void outputElement.offsetWidth; // access an element to force animation reset
     };
     document.body.style.animation = "fade 2s ease-in-out 0.5s 1";
+    
     sleep(2900).then(() => {document.body.style.animation = "";});
     return mult;
 };
 
 
 document.getElementById("rollerB").addEventListener("click", () => {
-    
-    let bet = parseInt(document.getElementById("moneyInput").value);
+    done = false;
+    let bet = parseFloat(parseFloat(document.getElementById("moneyInput").value).toFixed(2));
     let times = parseInt(document.getElementById("timesInput").value);
     if (isNaN(times)) {
         alert("Please enter a valid number of times to roll");
@@ -98,6 +118,7 @@ document.getElementById("rollerB").addEventListener("click", () => {
     } else if (!running) {
         console.log("bet: ", bet);
         money -= bet*times;
+        money = parseFloat(money.toFixed(2));
         document.getElementById("moneyOutput").innerHTML = "Money: " + money;
     }
     if (running) {return;};
@@ -127,7 +148,8 @@ document.getElementById("rollerB").addEventListener("click", () => {
         Roll(bet,div).then(multiplier => {
             console.log("multiplier: ", multiplier);
             
-            totalRevenue += bet * multiplier
+            totalRevenue += bet * multiplier;
+            done = true;
         });
     }
     
@@ -135,16 +157,16 @@ document.getElementById("rollerB").addEventListener("click", () => {
     for (let i = 0; i < 50; i ++) {
         totalTime += 30 * ((0.15)*(i^2) + 2);
     };
-    sleep(totalTime + 1500).then(() => {
+    checkFlag("done", () => {
         let target = document.getElementById("chancesT");
         bar = document.createElement("hr");
         document.body.insertBefore(bar,target);
         bar.style.animation = "width 0.5s linear 0s 1";
-        sleep(510).then(() => {
+        sleep(710).then(() => {
             totalRevenue = parseFloat(totalRevenue.toFixed(2));
             console.log(typeof(totalRevenue), " ", totalRevenue);
             money += totalRevenue;
-            document.getElementById("moneyOutput").innerHTML = "Money: " + money;
+            document.getElementById("moneyOutput").innerHTML = "Money: " + money.toFixed(2);
             total = document.createElement("div");
             total.className = "mult";
             total.innerHTML = (bet*times) + " x " + ((totalRevenue / (bet*times)).toFixed(4)) + " = " + totalRevenue;
@@ -153,10 +175,7 @@ document.getElementById("rollerB").addEventListener("click", () => {
             bar.insertAdjacentElement("afterend",total);
             running = false;
         });
-        
-        
-        
-    })
+    });
     
     
     
